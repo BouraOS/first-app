fe73a6c287c5af50393fc5c99e4988bb2ea2d797
BouraOS <mastershit32@gmail.com>
2023-07-25 01:51:19 +0100

    remove routes from app

--- src/App.js
+++ src/App.js
@@ -2,33 +2,12 @@ import 'normalize.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header'
+import FixturesLayout from './Components/Fixtures/Layouts/FixturesLayout';
 import './Components/Fixtures/FixStyle.css'
 import './App.css';
 
-import {
-    createBrowserRouter, 
-    createRoutesFromElements,
-    Route, 
-    RouterProvider
-  } from 'react-router-dom'
 
-// pages
-import Hier from './Components/Fixtures/pages/Hier'
-import Today from './Components/Fixtures/pages/Today';
-import Tomorrow from './Components/Fixtures/pages/Tomorrow';
 
-// layouts
-import FixturesLayout from './Components/Fixtures/Layouts/FixturesLayout';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<FixturesLayout />}>
-      <Route index element={<Today />} />
-      <Route path="hier" element={<Hier />} />
-      <Route path="demain" element={<Tomorrow />}/>
-    </Route>
-  )
-)
 
 function App() {
   return (
@@ -36,7 +15,7 @@ function App() {
         <header className="App-header">
           <Header/>
         </header>
-        <RouterProvider router={router} />
+        <FixturesLayout/>
     </div>
   );
 }
